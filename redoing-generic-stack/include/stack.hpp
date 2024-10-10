a067100fe4d94a74a6dcc87e0c025c78626ab43c
HarshaGD1997 <harshagd695@gmail.com>
2024-10-10 16:47:57 +0530

    redoing generic-stack incomplete

--- redoing-generic-stack/include/stack.hpp
+++ redoing-generic-stack/include/stack.hpp
@@ -1,6 +1,6 @@
 
 typedef struct{
-	void *elem;
+	void *elems;
 	int elemSize;
 	int allocatedLen;
 	int logicalLen;
@@ -8,6 +8,7 @@ typedef struct{
 
 }stack;
 
+void StringFree(void *elem);
 void StackNew(stack *s, int elemSize, void (*freefn)(void*)); 
 void StackPush(stack *s, void *elemAddr);
 void StackPop(stack *s, void *elemAddr);
